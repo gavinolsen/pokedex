5eec5e13c0cea4d607f8a0f8999c857e71a4a76e
Gavin Olsen <ols15015@byui.edu>
2017-04-11 10:03:17 -0600

    setup pokemonVC

---
+++ pokedex/PokemonViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PokemonViewController.swift
+//  pokedex
+//
+//  Created by Gavin Olsen on 4/11/17.
+//  Copyright © 2017 Gavin Olsen. All rights reserved.
+//
+
+import UIKit
+
+class PokemonViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    
+    
+
+}
